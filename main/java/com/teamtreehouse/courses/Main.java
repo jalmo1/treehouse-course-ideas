420b3450adac7c042aca3c150ea1a98e2e4809b1
jalmo1 <joshuarosamktp4@gmail.com>
2022-05-02 10:51:08 -0500

    First commit

---
+++ main/java/com/teamtreehouse/courses/Main.java
@@ -0,0 +1,13 @@
+package com.teamtreehouse.courses;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.get;
+
+public class Main {
+    public static void main(String[] args) {
+        get("/", (req, res) -> {
+            return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
